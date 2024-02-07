4df66f00140a2ae1706a24e5fa51c6c11f0cda4e
Hiroyuki Sasaki <sasaki@yoshida-g.ac.jp>
2024-02-07 11:21:36 +0900

    first commit

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask,render_template,redirect
+
+
+app = Flask(__name__, static_folder="./static/")
+
+# 自己紹介Webページを作成しましょう
+
+@app.route('/')
+def loginform():
+
+    return render_template('index.html')
+
+@app.route('/link1')
+def link1():
+    return render_template('link.html')
+
+@app.route('/link2')
+def link2():
+    return redirect('/')
+
+if __name__=='__main__':
+    app.run(host="0.0.0.0",port=80,debug=False)
